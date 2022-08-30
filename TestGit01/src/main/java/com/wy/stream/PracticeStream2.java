2c3866ffc15bcc60db2c6ef74a625f214ce75e4f
wangyu <1340694889@qq.com>
2022-08-30 10:00:36 +0800

    修改了PracticeStream2类

--- TestGit01/src/main/java/com/wy/stream/PracticeStream2.java
+++ TestGit01/src/main/java/com/wy/stream/PracticeStream2.java
@@ -2,10 +2,7 @@ package com.wy.stream;
 
 import com.wy.entity.Student;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PracticeStream2 {
@@ -15,18 +12,34 @@ public class PracticeStream2 {
 
         List<Student> stuList = new ArrayList<Student>();
         stuList.add(new Student(20160001, "KongMing", 20, 1, "土木工程", "武汉大学"));
-        stuList.add(new Student(20160002, "BoYue", 21, 2, "信息安全", "武汉大学"));
+        stuList.add(new Student(20160002, "BoYue", 15, 2, "信息安全", "武汉大学"));
         stuList.add(new Student(20160003, "XuanDe", 22, 3, "经济管理", "武汉大学"));
-        stuList.add(new Student(20160004, "YunChang", 21, 2, "信息安全", "武汉大学"));
-        stuList.add(new Student(20161001, "YiDe", 21, 2, "机械与自动化", "华中科技大学"));
+        stuList.add(new Student(20160004, "YunChang", 16, 2, "信息安全", "武汉大学"));
+        stuList.add(new Student(20161001, "YunChang2", 16, 2, "信息安全", "武汉大学"));
+        stuList.add(new Student(20161001, "YiDe", 13, 2, "机械与自动化", "华中科技大学"));
         stuList.add(new Student(20161002, "YuanZhi", 23, 4, "土木工程", "华中科技大学"));
         stuList.add(new Student(20161003, "FengXiao", 23, 4, "计算机科学", "华中科技大学"));
         stuList.add(new Student(20162001, "ZhongMou", 22, 3, "土木工程", "浙江大学"));
-        stuList.add(new Student(20162002, "LuSu", 23, 4, "计算机科学", "浙江大学"));
-        stuList.add(new Student(20163001, "DingFeng", 24, 5, "土木工程", "南京大学"));
+        stuList.add(new Student(20162002, "LuSu", 23, 5, "计算机科学", "浙江大学"));
+        stuList.add(new Student(20163001, "DingFeng", 13, 5, "土木工程", "南京大学"));
+
+        // 根据Grade和Age两个字段进行分组。
+
+        Map<Integer, Map<String, List<Student>>> map = stuList.stream()
+                .collect(Collectors.groupingBy(Student::getGrade, Collectors.groupingBy(student -> {
+                    return student.getAge() >= 18 ? "成年" : "未成年";
+                })));
+        map.forEach((k, v) -> {
+            System.out.println(k);
+            v.forEach((k1, v1) -> {
+                System.out.println("\t" + k1 + ":" + v1);
+            });
+        });
 
 
-        // 1.汇总专业名为土木工程的总人数
+        System.out.println("================================================================");
+
+        /*// 1.汇总专业名为土木工程的总人数
         long count = stuList.stream()
                 .filter(student -> student.getMajor().equals("土木工程")).count();
         // 2. 汇总专业名为"土木工程"的学生的姓名集合;
@@ -47,6 +60,13 @@ public class PracticeStream2 {
         System.out.println(collect1);
 
 
+        System.out.println("================================================================");
+
+
+        System.out.println("最大年龄：=" + stuList.stream().max(Comparator.comparing(Student::getAge)).orElse(null));
+
+        System.out.println("平均年龄：=" + stuList.stream().mapToInt(Student::getAge).summaryStatistics().getAverage());
+*/
     }
 
 
